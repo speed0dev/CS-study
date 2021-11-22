c046fd0b0a258059d3ba3b21128852fca2aa7425
speed0dev <speed0dev@gmail.com>
2021-11-23 02:50:48 +0900

    test

---
+++ fireServer/AppRun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace fireServer
+{
+    class AppRun
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Server]start --------------------");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
