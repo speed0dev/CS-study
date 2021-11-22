f0d9c9bf30cb1952ee757a1e1ffb9e30031cc2d6
speed0dev <speed0dev@gmail.com>
2021-11-23 02:47:05 +0900

    test

---
+++ ConsoleApp1/app/AClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.app
+{
+    public class AClass
+    {
+        public void call()
+        {
+            Console.WriteLine("AClass App");
+        }
+    }
+}
